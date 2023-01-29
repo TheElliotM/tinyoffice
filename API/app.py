f6e6b4d680cdf3459e6ec16061a5657c33eb0ae8
Andrew800 <andrewzhengschool@gmail.com>
2023-01-28 21:55:40 -0600

    Iteration 2

--- API/app.py
+++ API/app.py
@@ -1,4 +1,4 @@
-from itertools import chain, combinations
+from itertools import chain, combinations, product
 
 # Returns the powerset of the list
 def powerset(iterable):
@@ -21,32 +21,21 @@ current = 0
 strengths = {1: 22, 2: 45, 3: 34, 4: 51, 5: 11, 6: 37, 7: 42, 8: 16, 9: 29, 10: 56, 11: 49}
 floors = {"A": 43, "B": 81, "C": 73, "D": 54, "E": 97}
 # No way preferences by team (starting at 0, so team 1 is 0, team 2 is 1, etc.)
-no_ways = {0: [5, 7, 9], 1: [4, 8, 9, 10], 2: [4, 5, 6, 8, 9, 10], 3: [2, 5, 6, 7, 8, 9, 11], 4: [6, 7, 8], 5: [2, 3, 4, 5, 9, 11], 6: [8, 9], 7: [3, 5, 6, 7, 9], 8: [3, 4, 6, 7, 8, 11], 9: [1, 3, 9], 10: [8]}
+no_ways = {1: [5, 7, 9], 2: [4, 8, 9, 10], 3: [4, 5, 6, 8, 9, 10], 4: [2, 5, 6, 7, 8, 9, 11], 5: [6, 7, 8], 6: [2, 3, 4, 5, 9, 11], 7: [8, 9], 8: [3, 5, 6, 7, 9], 9: [3, 4, 6, 7, 8, 11], 10: [1, 3, 9], 11: [8]}
 
-for i in range(11):
+for i in range(1, 12):
     #temps = map(int, (input(f"No way for team {i + 1}: ").split()))
-    temps = no_ways[i]
-    for temp in temps:
+    nos = no_ways[i]
+    for no in nos:
         try:
-            teams[i].remove(temp)
+            teams[i - 1].remove(no)
         except:
             continue
-        if i + 1 in teams[temp - 1]:
+        if i in teams[no - 1]:
             try:
-                teams[temp - 1].remove(i + 1)
+                teams[no - 1].remove(i)
             except:
                 continue
-
-# single = True
-# partner = 0
-# while (single):
-#     for i in range(11):
-#         if i == 10 and len(teams[i]) != 1 and single == True:
-#             single = False
-#         else:
-#             if len(teams[i]) == 1:
-#                 partner = teams[i][0]
-#                 teams[partner - 1] = [i + 1]
 
 # floors_final is the list of all possible teams on all floors
 # floors_final[index] is the list of all possible teams on the indexth floor (starting from index = 0)
@@ -63,11 +52,15 @@ for index, floor in enumerate(floors_final):
         subsets = x(powerset(temp_floor), i + 1)
         temp_subsets = subsets[:]
         for subset in subsets:
+            invalid = False
             sum = 0
             for element in subset:
                 sum += strengths[element]
-            if sum > floors[chr(65 + index)] or sum < floors[chr(65 + index)] * 0.25:
+                if element in chain(*[no_ways[temp] for temp in subset]):
+                    invalid = True
+            if invalid == True or sum > floors[chr(65 + index)] or sum < floors[chr(65 + index)] * 0.25:
                 temp_subsets.remove(subset)
+                invalid = False
         subsets = temp_subsets
         floors_final[index][i] = subsets
 
@@ -76,11 +69,52 @@ for index, floor in enumerate(floors_final):
     #     print(f"Team {i + 1} can be with {floors_final[index][i]}")
     # print()
 
-print("ALL POSSIBLE FLOOR COMBINATIONS")
+all_combinations = []
+floor_combinations = []
+#print("ALL POSSIBLE FLOOR COMBINATIONS")
 for index_floor, floor in enumerate(floors_final):
-    print(f"------FLOOR {chr(65 + index_floor)}------")
+    #print(f"------FLOOR {chr(65 + index_floor)}------")
+    floor_combinations = []
     for index_team, team in enumerate(floor):
         if team != []:
             for index_combination, combination in enumerate(team):
-                print(f"\t{combination}")
+                #print(f"\t{combination}")
+                floor_combinations.append(combination)
+    all_combinations.append(floor_combinations)
 
+buildings = set(product(*all_combinations))
+space_scores = []
+like_scores = []
+for building in buildings.copy():
+    teams_exist = [False, False, False, False, False, False, False, False, False, False, False, False]
+    duplicate = False
+    space_score = 0
+    like_score = 0
+    for floor in building:
+        for team in floor:
+            if teams_exist[team - 1] == True:
+                buildings.remove(building)
+                duplicate = True
+                break
+            else:
+                teams_exist[team - 1] = True
+                space_score += strengths[team]
+        if duplicate == True:
+            break
+        # if teams do not like each other:
+            # set duplicate to true
+        # for ys in yss:
+        #     for y in ys:
+        #         if y in xs:
+        #             duplicate = True
+        #             break
+        #     if duplicate == True:
+        #         break
+
+    if duplicate == False:
+        space_score = space_score / 348
+        space_scores.append(building + (space_score,))
+    #else:
+        #print(f"Teams: {xs}")
+space_scores.sort(key = lambda x: x[-1])
+print(space_scores[-1])
